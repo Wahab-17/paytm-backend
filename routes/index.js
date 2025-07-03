6357ba4a305788550c7b97c6bc0527bfb26fa6ac
Mohd Abdul Wahab <150377076+Wahab-17@users.noreply.github.com>
2025-07-03 11:32:55 +0530

    added

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ import userRouter from "./user.js ";
 import accountrouter from "./accounts.js";
 
 const Router = express.Router();
+
+console.log("✅ Mounting /user routes...");
+
 Router.use("/user",userRouter);
+
+
+
 console.log("✅ Mounting /account routes...");
 Router.use("/account",accountrouter);
 
